3f59a7de63bc1fb4a340b9d1fbaf5239413a76b4
naresh5796 <nareshm5796@gmail.com>
2024-03-05 00:34:22 +0530

    bg-changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 const Home = () => {
     const path = 'https://media.istockphoto.com/id/1403087795/de/video/mann-scannen-von-gesichtsmerkmalen-futuristische-3d-identifikation-konzept-der-gesichts-id.mp4?s=mp4-640x640-is&k=20&c=VyaZ_L2lXKMKyWtZ-IafNu5gw5ROpCNZ1Wu2FzUjWjQ='
-    return <div className="home-container">
-                <video autoplay controls src={path} type="video/mp4" width="40%"/>
-                <h1>
-                    heloo
-                </h1>
-            </div>
+    return <div className='fluid-container'>
+            <div className="container home-container">
+                    <video autoPlay controls src={path} type="video/mp4" width="40%"/>
+                </div>
+        </div>
   };
   
   export default Home;
