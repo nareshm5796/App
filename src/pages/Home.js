f57ad36d18a5139737ffd319b34cfd0d412f5ad9
nareshm5796 <147257209+nareshm5796@users.noreply.github.com>
2024-03-05 14:42:13 +0530

    Header set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Home = () => {
-    const path = 'https://media.istockphoto.com/id/1403087795/de/video/mann-scannen-von-gesichtsmerkmalen-futuristische-3d-identifikation-konzept-der-gesichts-id.mp4?s=mp4-640x640-is&k=20&c=VyaZ_L2lXKMKyWtZ-IafNu5gw5ROpCNZ1Wu2FzUjWjQ='
+    
     return <div className='fluid-container'>
             <div className='container'>
                 <div className='row '>
@@ -13,8 +13,6 @@ const Home = () => {
                         <h1>right</h1>
                 </div>
                 </div>
-                
-
             </div>
         </div>
   };
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,11 +5,16 @@ const Layout = () => {
       <>
         <nav>
           <ul>
-            <li><Link to="/" className="link1">Home</Link></li>
+            <li><Link to="/" className="link1"><img
+              src="https://bookshelf.themerex.net/wp-content/uploads/2021/07/Logo-invert.png"
+              class="img-fluid rounded-top w-50"
+              alt=""
+            />
+            </Link></li>
           </ul>
           <ul>
             <li><Link to="/Login" className="link1">Login</Link></li>
-            <li><Link to="/Signup" className="link1">Signup</Link></li>
+            <li><Link to="/Signup" className="link1">Rigister Now</Link></li>
           </ul>
         </nav>
         <Outlet />
